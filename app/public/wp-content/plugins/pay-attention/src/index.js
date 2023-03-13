ca6ffbbcf650a74cd7d42b6d0fb8fe6e0ac2e374
denis_levchenko <tranzi90@gmail.com>
2023-03-13 22:00:23 +0300

    work on quiz answers crud

--- app/public/wp-content/plugins/pay-attention/src/index.js
+++ app/public/wp-content/plugins/pay-attention/src/index.js
@@ -6,7 +6,8 @@ wp.blocks.registerBlockType("plugin/are-you-paying-attention", {
     icon: "smiley",
     category: "common",
     attributes: {
-
+        question: {type: "string"},
+        answers: {type: "array", default: ["dlc", "ng+"]}
     },
     edit: EditComponent,
     save: function () {
@@ -14,26 +15,56 @@ wp.blocks.registerBlockType("plugin/are-you-paying-attention", {
     }
 })
 
-function EditComponent(props) {
+function EditComponent({ attributes, setAttributes }) {
+    function updateQuestion(value) {
+        setAttributes({question: value})
+    }
+
+    function deleteAnswer(idxToDelete) {
+        const newAnswers = attributes.answers.filter((x, idx) => idx !== idxToDelete)
+
+        setAttributes({answers: newAnswers})
+    }
+
     return (
         <div className="paying-attention-edit-block">
-            <TextControl label="Question:" />
-            <p>Answers:</p>
-            <Flex>
-                <FlexBlock>
-                    <TextControl />
-                </FlexBlock>
-                <FlexItem>
-                    <Button>
-                        <Icon icon="star-empty" />
-                    </Button>
-                </FlexItem>
-                <FlexItem>
-                    <Button>
-                        Delete
-                    </Button>
-                </FlexItem>
-            </Flex>
+            <TextControl
+                label="Question:"
+                value={attributes.question}
+                onChange={updateQuestion}
+                style={{fontSize: "20px"}}
+            />
+            <p style={{fontSize: "13px", margin: "20px 0 8px 0"}}>Answers:</p>
+            {attributes.answers.map(function (answer, idx) {
+                return (
+                    <Flex>
+                        <FlexBlock>
+                            <TextControl value={answer} onChange={newValue => {
+                                const newAnswers = attributes.answers.concat([])
+                                newAnswers[idx] = newValue
+                                setAttributes({answers: newAnswers})
+                            }} />
+                        </FlexBlock>
+                        <FlexItem>
+                            <Button>
+                                <Icon icon="star-empty" className="mark-as-correct" />
+                            </Button>
+                        </FlexItem>
+                        <FlexItem>
+                            <Button
+                                isLink
+                                className="attention-delete"
+                                onClick={() => deleteAnswer(idx)}
+                            >
+                                Delete
+                            </Button>
+                        </FlexItem>
+                    </Flex>
+                )
+            })}
+            <Button isPrimary onClick={() => {
+                setAttributes({answers: attributes.answers.concat([""])})
+            }}>Add another answer</Button>
         </div>
     )
 }
